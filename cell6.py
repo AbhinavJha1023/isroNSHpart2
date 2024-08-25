1b1cf0e7634e2eb3b93e7a91fa62f8c0be80fdfa
Abhinav R Jha <bt23csd053@iiitn.ac.in>
2024-08-25 15:08:28 +0530

    Update cell6.py

--- cell6.py
+++ cell6.py
@@ -1,1 +1,7 @@
-
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x='Energy_Production_MWh', y='Energy_Consumption_MWh', hue='Type_of_Renewable_Energy', palette='viridis', data=df)
+plt.title('Energy Production vs. Consumption')
+plt.xlabel('Energy Production (MWh)')
+plt.ylabel('Energy Consumption (MWh)')
+plt.legend(title='Type of Renewable Energy', labels=['Solar', 'Wind', 'Hydroelectric', 'Geothermal', 'Biomass', 'Tidal', 'Wave'])
+plt.show()
