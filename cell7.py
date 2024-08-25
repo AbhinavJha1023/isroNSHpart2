8fa60ccae4ef6cbb788624ff944a5c28c366d080
Abhinav R Jha <bt23csd053@iiitn.ac.in>
2024-08-25 15:08:44 +0530

    Update cell7.py

--- cell7.py
+++ cell7.py
@@ -1,1 +1,7 @@
-
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='Type_of_Renewable_Energy', y='Storage_Efficiency_Percentage', data=df, palette='viridis')
+plt.title('Storage Efficiency of Different Types of Renewable Energy')
+plt.xlabel('Type of Renewable Energy')
+plt.ylabel('Storage Efficiency (%)')
+plt.xticks(ticks=[0, 1, 2, 3, 4, 5, 6], labels=['Solar', 'Wind', 'Hydroelectric', 'Geothermal', 'Biomass', 'Tidal', 'Wave'])
+plt.show()
