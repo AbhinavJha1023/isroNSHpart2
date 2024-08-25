6553b69a2b1d01dcb0fd732464c9af1ac66e0685
Abhinav R Jha <bt23csd053@iiitn.ac.in>
2024-08-25 15:09:27 +0530

    Update cell9.py

--- cell9.py
+++ cell9.py
@@ -1,1 +1,8 @@
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Type_of_Renewable_Energy', y='Jobs_Created', data=df, ci=None, palette='viridis')
+plt.title('Number of Jobs Created by Different Types of Renewable Energy')
+plt.xlabel('Type of Renewable Energy')
+plt.ylabel('Number of Jobs Created')
+plt.xticks(ticks=[0, 1, 2, 3, 4, 5, 6], labels=['Solar', 'Wind', 'Hydroelectric', 'Geothermal', 'Biomass', 'Tidal', 'Wave'])
+plt.show()
 
